7d1e35e5e255c563ce09f8e69f5d0a718cc6bc0a
Your Name <j.e.forero.romero@gmail.com>
2015-05-13 17:30:21 +0200

    removing unised files

--- data/BigMD_Planck/select_MDPL.sh
+++
@@ -1,4 +0,0 @@
-for i in 0 1 2 3 4 5 
-do
-   awk < /srv/cosmdata/multidark/BigMD_3840_Planck1/BDM/CatshortW.0080.0$i.DAT '{if($8>5.0E11 && $15==0)print $0}' > BigMD_BDMW_snap_80_Mvir_5E11_0$i.dat
-done
